45f857388c526a9c850decdb11617fedcf48ad18
Silence0206 <732379020@qq.com>
2016-07-15 14:55:14 +0800

    模拟登录修改完毕 但现网页乱码问题未解决

---
+++ Learn_login11.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from urllib import request, parse
+import http.cookiejar
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+print('Login to weibo.cn...')
+url='https://passport.weibo.cn/sso/login'
+
+login_data = parse.urlencode([
+    ('username', "732379020@qq.com"),
+    ('password', "5842HERO"),
+    ('entry', 'mweibo'),
+    ('client_id', ''),
+    ('savestate', '1'),
+    ('ec', ''),
+    ('pagerefer', 'https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F')
+])
+filename = 'cookie.txt'
+data=login_data.encode('utf-8')
+cookie = http.cookiejar.CookieJar()
+pro = urllib.request.HTTPCookieProcessor(cookie)
+openner=urllib.request.build_opener(pro)
+openner.addheaders=[('Origin', 'https://passport.weibo.cn')]
+openner.addheaders=[('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')]
+openner.addheaders=[('Referer', 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')]
+
+with openner.open(url ,login_data.encode('utf-8')) as f:
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', f.read().decode('utf-8'))
+
+response=openner.open("http://m.weibo.cn/k/%E9%99%88%E6%99%93%E8%B5%B5%E4%B8%BD%E9%A2%96?from=feed")
+html = response.read().decode('utf-8')
+soup = BeautifulSoup(html,"html.parser")
+print(soup)
+
+
+
+
