4221c1cbb17e2405ee57a5d94b4c7b81b95c5837
Silence0206 <732379020@qq.com>
2016-07-26 01:28:04 +0800

    cunyixia

--- Dianping_All_Comments.py
+++ Dianping_All_Comments.py
@@ -10,13 +10,26 @@ import logging
 SleepNum = 1     # 抓取页面的间隔时间，可为0
 headers = {
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding":"gzip, deflate, sdch",
+    "Connection":"keep-alive",
     "Accept-Language" :"zh-CN,zh;q=0.8",
     "Host": "www.dianping.com",
     "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
     'Referer': "http://www.dianping.com/shanghai/life",
-    'Cookie':'_hc.v=00182c04-e7c3-0953-dc3a-0c507f60bdb0.1469080663; __utma=205923334.275296798.1469082376.1469082376.1469109403.2; __utmz=205923334.1469082376.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); PHOENIX_ID=0a650c81-15610b18a16-8a1918; _dp.ac.v=d0884a6b-b29b-4748-90a4-4f71e1bc8535; dper=4dcc94bf30f951ac9a83cfdc9df8d32056c8aab3926eb9220b40603ad7c444ff; ll=7fd06e815b796be3df069dec7836c3df; ua=732379020%40qq.com; ctu=b33af298f0f018b44702e07460c6750f2148460b1f3f12d2a2a92cf0e629162e; s_ViewType=10; aburl=1; cy=1; cye=shanghai; _hc.s=\"\\\"00182c04-e7c3-0953-dc3a-0c57f60bdb0b.1469080663.1469158888.1469159378\\\"\"'
+    # 'Cookie':'_hc.v=00182c04-e7c3-0953-dc3a-0c57f60bdb0b.1469080663; __utma=1.273426386.1469369064.1469369064.1469369064.1; __utmz=1.1469369064.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); dper=5529c55bfbd478e8c1613429bfde641841a70874a3484cd0259f2c34696bdc02; ua=15651383078; PHOENIX_ID=0a017429-1562227b555-ae953b6; ll=7fd06e815b796be3df069dec7836c3df; JSESSIONID=B596AEA7C9E6A9AF4B51AFD1AC85FFC9; aburl=1; cy=1; cye=shanghai'
+    'Cookie':'m_rs=5a23972d-921a-4d46-9ff8-d385d3381dbd; _hc.v=00182c04-e7c3-0953-dc3a-0c57f60bdb0b.1469080663; __utma=1.273426386.1469369064.1469369064.1469369064.1; __utmz=1.1469369064.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); dper=79e1190dfef09628df10db9edd2c9590a25d955f69a85bfe684d4a4ff5d2ef02; ua=13816174065; PHOENIX_ID=0a65026a-1562272a84d-126c111; ll=7fd06e815b796be3df069dec7836c3df; s_ViewType=10; JSESSIONID=6043782DB8A392CA09529A51163D3CA0; aburl=1; cy=1; cye=shanghai'
 }
 proxies = {
+"http":"180.169.5.20:1920",
+"http":"119.188.94.145:80",
+"http":"180.161.16.166:8118",
+"http":"183.161.248.119:8118",
+"http":"60.185.209.157:8998",
+"http":"120.36.164.143:8118",
+"http":"122.239.26.137:8998",
+"http":"123.7.177.20:9999",
+"http":"119.165.239.81:8118",
+"http":"58.214.229.228:8118",
 "http":"123.119.18.138:8118",
 "http":"61.52.247.229:3128",
 "http":"183.129.178.14:8080",
@@ -93,7 +106,7 @@ def find_comment_onePage(url):
     if("review_more" not  in url):
         url += "/review_more"
     try:
-        req = requests.get(url, headers=headers, proxies=proxies)
+        req = requests.get(url, headers=headers)
         soup = BeautifulSoup(req.text, 'lxml')
         # 页码
         pageDiv = soup.find(class_="Pages").find(class_="Pages")
@@ -106,8 +119,8 @@ def find_comment_onePage(url):
         comts = soup.find(class_="comment-list").find("ul")
     except BaseException as e:
         print(url, "打开出错休息2秒 错误原因：", e)
-        error_resIdLog = open('error_resId.txt', 'a')
-        error_resIdLog.writelines("打开页面失败的店铺："+resId+"时间："+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"原因：" + str(e) + "=========================\n")
+        error_resIdLog = open('fail_resId.txt', 'a')
+        error_resIdLog.writelines(url+"打开页面失败的店铺："+resId+"时间："+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"原因：" + str(e) + "=========================\n")
         logger.error(e)
         time.sleep(1)
         return ("")
@@ -170,6 +183,7 @@ def find_comment_onePage(url):
             logger.error(e)
             time.sleep(1)
     print("第", curr_pageNum, "页爬取完成")
+    time.sleep(2)
     if (pageDiv is not None):
         next_page = pageDiv.find(class_="NextPage")
     else:
@@ -183,13 +197,14 @@ def find_comment_onePage(url):
 def find_all_page(url):
     link = url
     while (link != ""):
+        print(find_comment_onePage(link))
         link = find_comment_onePage(link)[0]
 
 def read_list(usr, pwd, db):
     try:
         conn1 = mysql.connector.connect(user=usr, password=pwd, database=db)
         cursor = conn1.cursor()
-        cursor.execute( 'SELECT * FROM restaurants where comm_num >50 order by  res_id limit 0,10')
+        cursor.execute( 'SELECT * FROM restaurants where comm_num >50 and flag=false order by  res_id limit 0,10')
         rows = cursor.fetchall()
         cursor.close()
         conn1.close()
@@ -198,27 +213,8 @@ def read_list(usr, pwd, db):
         print("出问题啦", e)
         return
 
-n=0
-create_database('root', '58424716', 'dianping')
-a=read_list('root', '58424716', 'dianping')
-for item in a :
-    region_id=item[0] #店铺id 店铺名称 店铺链接
-    region_name=item[1]
-    region_link =item[2]
-    print("========开始爬取 店铺id:",region_id,"店铺名称：",region_name,"店铺链接：",region_link)
-    try:
-        find_all_page(region_link)
-    except BaseException as e:
-        print("爬取该店铺出错啦",e)
-        logger.error(e)
-        error_resIdLog = open('error_resId.txt', 'a')
-        error_resIdLog.writelines("时间："+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"做标记失败的店铺："+region_id+"   "+region_link+"\n")
-    set_flag(region_id)
-    time.sleep(2)
-
-
-
-# find_comment_onePage("http://www.dianping.com/shop/3259888/review_more")
+# create_database('root', '58424716', 'dianping')
+# a=read_list('root', '58424716', 'dianping')
 # for item in a :
 #     region_id=item[0] #店铺id 店铺名称 店铺链接
 #     region_name=item[1]
@@ -226,28 +222,13 @@ for item in a :
 #     print("========开始爬取 店铺id:",region_id,"店铺名称：",region_name,"店铺链接：",region_link)
 #     try:
 #         find_all_page(region_link)
+#         set_flag(region_id)
 #     except BaseException as e:
 #         print("爬取该店铺出错啦",e)
 #         logger.error(e)
 #         error_resIdLog = open('error_resId.txt', 'a')
-#         error_resIdLog.writelines("做标记失败的店铺："+region_id+"   "+region_link+"\n")
-#     set_flag(region_id)
-#     n +=1
-#     if(n>10):
-#         break
-#     time.sleep(3)
+#         error_resIdLog.writelines("时间："+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"findall出问题："+region_id+"   "+region_link+"\n")
+#     time.sleep(2)
 
-
-
-
-
-
-
-
-        # find_all_page("http://www.dianping.com/shop/10333309")
-
-        # # set_flag("3d1dd1")
-# a=find_comment_onePage("http://www.dianping.com/shop/3259888/review_more?pageno=1106")
-# print(a)
-# insert_rescomments("2827101594", "3259888", "2032804", "花卷的妈咪", True, 45, 10, 0,
-#                    0, 0, 0, "真的很", "0000-00-00 00:00:00", "2016-07-24 23:36:53")
+req = requests.get(" http://www.dianping.com/shop/10329816", headers=headers,proxies=proxies)
+soup = BeautifulSoup(req.text, 'lxml')
